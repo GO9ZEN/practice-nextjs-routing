a74a49d29465b0b5e42f949a0c123105933718b3
GO9ZEN <sarasasilva2002@gmail.com>
2024-04-30 09:21:52 +0530

    first commit

---
+++ src/app/products/[productId]/layout.tsx
@@ -0,0 +1,23 @@
+function getRandomInt(count: number) {
+  return Math.floor(Math.random() * count);
+}
+
+export default function ProductDetailsLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  // const random = getRandomInt(2);
+
+  // if (random == 1) {
+  //   throw new Error("Error loading review");
+  // }
+
+  return (
+    <>
+      {children}
+      <h2>Features product</h2>
+      {/* Carousel here */}
+    </>
+  );
+}
